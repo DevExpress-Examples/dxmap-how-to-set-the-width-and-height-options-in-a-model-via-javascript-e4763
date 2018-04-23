e3c67ae0300c282a3e18b9ab21f2ef9d57259c66
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/dxSample_KA18813_Model/DxSample/views/index.js
@@ -0,0 +1,10 @@
+﻿DxSample.Index = function (params) {
+    function ViewModel() {
+        this.mapOptions = {
+            width: 1200,
+            height: 700
+        };
+    }
+
+    return new ViewModel();
+};
